82feef73e3058c9277095080d77d6eb92ae42901
kasun-a <76146177+kasun-a@users.noreply.github.com>
2023-10-27 08:31:03 +0530

    auth

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 import os
 import json
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, auth
 
 # Initialize Firebase Admin SDK with your service account credentials
 credPull = credentials.Certificate("./keys/pull-key.json")
@@ -15,8 +15,8 @@ dbPull = firestore.client(app=pullApp)
 dbPush = firestore.client(app=pushApp)
 
 
-def pullData(collection_list,version):  
-    os.mkdir(f"data/version_{version}")
+def pull_collection_data(collection_list,version):  
+    os.mkdir(f"data/version_{version+1}")
     for collection_name in collection_list:
         collection_ref = dbPull.collection(collection_name)
         documents = collection_ref.stream()
@@ -26,13 +26,13 @@ def pullData(collection_list,version):
         for document in documents:
             data_dict[document.id] = document.to_dict()
 
-        with open(f"data/version_{version}/{collection_name}.json", "w") as json_file:
+        with open(f"data/version_{version+1}/{collection_name}.json", "w") as json_file:
             json.dump(data_dict, json_file, indent=4)
             
         print(f"{collection_name}: Dumped successfully")       
 
 
-def pushData(collection_list,version):
+def push_collection_data(collection_list,version):
     for collection_name in collection_list:
         with open(f"data/version_{version}/{collection_name}.json", "r") as json_file:
             data_dict = json.load(json_file)
@@ -46,11 +46,57 @@ def pushData(collection_list,version):
         print(f"{collection_name}: Uploaded successfully") 
 
 
+def pull_auth_data():
+    try:
+        # Retrieve all users from Firebase Authentication
+        users = auth.list_users(app=pullApp)
+
+        user_data = []
+        for user in users.users:
+            user_info = user._data
+            user_data.append(user_info)
+
+        # Write user data to a JSON file
+        with open(f"data/auth.json", 'w') as json_file:
+            json.dump(user_data, json_file, indent=4)
+
+        print(f"Total of {len(user_data)} users exported")
+        
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        
+        
+def push_auth_data():
+    try:
+        # Open the JSON file and load user data
+        with open(f"data/auth.json", 'r') as json_file:
+            user_data = json.load(json_file)
+
+        # Upload each user to Firebase Authentication
+        for user_info in user_data:
+            user = auth.create_user(
+                email=user_info.get('email'),
+                password=user_info.get('password'),
+                display_name=user_info.get('displayName'),
+                phone_number=user_info.get('phoneNumber')
+                # Add more user attributes as needed
+            )
+            print(f"User {user.uid} uploaded to Firebase Authentication")
+
+        print(f"{len(user_data)} Users uploaded successfully")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
 if __name__ == "__main__":
     index=len(next(os.walk('data'))[1])
     
     # Specify the Firestore collection name
     collectionList = ["apps", "food_item_collection","food_collection"]
     
-    pullData(collectionList,index+1)
-    # pushData(["apps"],index)
+    # Firestore
+    # pull_collection_data(collectionList,index)
+    # push_collection_data(["apps"],index)
+    
+    #auth
+    pull_auth_data()
+    
